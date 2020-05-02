dfa788006a585543c944a9bd825d032004808ab8
damian <pixiecoda@gmail.com>
2020-05-02 12:26:27 -0400

    online command !!

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const { get } = require("snekfetch");
-const { token } = require("./config.json");
+const { token, prefix, hypixel } = require("./config.json");
 
 client.once("ready", () => {
     console.log("Hello, world.");
 });
 
 client.on("message", async(message) => {
-    
+    if (message.content === "hypixel online" || message.content === "Hypixel Online") {
+        let { body } = await get(`https://api.hypixel.net/playerCount?key=${hypixel}`);
+        message.channel.send(`There are **${body.playerCount.toLocaleString()} online** members.`);
+    }
 });
 
 client.login(token);
