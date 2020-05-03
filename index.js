ae946f041c645f3feb1cc663e6324b7ac64ab6ff
damian <pixiecoda@gmail.com>
2020-05-02 23:55:59 -0400

    ??

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const client = new Discord.Client();
 const { get } = require("snekfetch");
 const { token, prefix, hypixel } = require("./config.json");
 
-client.once("ready", () => {
+client.once("ready", async() => {
     console.log("Hello, world.");
+    let { body } = await get(`https://api.hypixel.net/playerCount?key=${hypixel}`);
+    client.user.setActivity(`${body.playerCount.toLocaleString()} Players`, {
+        type: "WATCHING"
+    });
 });
+
+async function update() {
+    let { body } = await get(`https://api.hypixel.net/playerCount?key=${hypixel}`);
+    client.user.setActivity(`${body.playerCount.toLocaleString()} Players`, {
+        type: "WATCHING"
+    });
+};
+
+setInterval(update, 10000);
 
 client.on("message", async(message) => {
     if (message.content === "hypixel online" || message.content === "Hypixel Online") {
