758acc23e67d7a57dca937feadb83d19783cf463
damian <pixiecoda@gmail.com>
2020-05-03 01:50:39 -0400

    here you go boomers

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,13 @@ const Discord = require("discord.js");
 module.exports = {
 	name: 'help',
 	description: 'List all available commands.',
-	execute(message) {
+	execute(client, message, args) {
 		let str = '';
 		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 		for (const file of commandFiles) {
 			const command = require(`./${file}`);
-			str += `Name: ${command.name}, Description: ${command.description} \n`;
+			str += `${command.name}: ${command.description} \n`;
 		}
 
 		const embed = new Discord.RichEmbed();
