27a8f714eb091a68878fae36d4fea77db5f2f109
damian <pixiecoda@gmail.com>
2020-05-03 00:16:36 -0400

    kek

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message, args) {
+	execute(client, message, args) {
 		message.channel.send(`:ping_pong: **Ping!** Latency is **${Math.round(client.ping)} milliseconds.**`);
 	},
 };
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on('message', async (message) => {
 	if (!client.commands.has(command)) return;
 
 	try {
-		client.commands.get(command).execute(message, args);
+		client.commands.get(command).execute(client, message, args);
 	}
 	catch (error) {
 		console.error(error);
