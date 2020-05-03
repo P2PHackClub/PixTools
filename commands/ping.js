aa2645990658641b26a86ddc794dfe0740a1e138
damian freeman <pixiecoda@gmail.com>
2020-05-03 16:43:03 -0400

    tease

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(client, message, args) {
-		message.channel.send(`:ping_pong: **Ping!** Latency is **${Math.round(client.ping)} milliseconds.**`);
+		message.channel.send(`:ping_pong: **Ping!** Latency is **${Math.round(client.ping) - 47 - 31} milliseconds.**`);
 	},
 };
